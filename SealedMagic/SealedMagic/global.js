2a935707eae2eaf122030484e51469dec12873ec
Kunzaite <rkpost87@gmail.com>
2014-03-16 23:41:36 +0100

    site structure
    site navigation changes done

---
+++ SealedMagic/SealedMagic/global.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    console.log(location.href);
+    var hreflist = location.href.toString().replace(".", "/").split('/');
+    console.log("x" + hreflist[4]);
+    $('#menu_' + hreflist[4]).addClass("chosen_menu");
+});
